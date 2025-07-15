fe6ef1fb8eff8b7969bf113cba98c1d4a6632263
cyinggg <118198699+cyinggg@users.noreply.github.com>
2025-07-15 16:09:40 +0000

    Update keepalive and .replit files
    :qw

--- keepalive.py
+++ keepalive.py
@@ -1,15 +1,13 @@
-from flask import Flask,render_template
+from flask import Flask
 from threading import Thread
 
 app = Flask(__name__)
+
 
 #Function returns 'ALive' while running the program
 @app.route('/')
 def home():
     return "Bot is running!", 200
-
-def run():
-    app.run(host='0.0.0.0', port=5000)
 
 def keep_alive():
     t = Thread(target=run)
